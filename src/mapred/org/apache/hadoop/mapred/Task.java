80347b38a9b99f792420ff50c47d795fb854bcbb
gndpig <gnd.scandal.pig@gmail.com>
2014-11-15 17:13:14 +0900

    修正

--- src/mapred/org/apache/hadoop/mapred/Task.java
+++ src/mapred/org/apache/hadoop/mapred/Task.java
@@ -431,9 +431,9 @@ abstract public class Task implements Writable, Configurable, Cloneable {
     out.writeBoolean(writeSkipRecs);
     out.writeBoolean(taskCleanup); 
     Text.writeString(out, user);
-    for (int i = 0; i < dataVolume.length; i++) {
-      out.writeInt(dataVolume[i]);    	
-    }
+//    for (int i = 0; i < dataVolume.length; i++) {
+//      out.writeInt(dataVolume[i]);    	
+//    }
   }
   
   public void readFields(DataInput in) throws IOException {
@@ -458,10 +458,10 @@ abstract public class Task implements Writable, Configurable, Cloneable {
       setPhase(TaskStatus.Phase.CLEANUP);
     }
     user = Text.readString(in);
-    int numReduceTasks = in.readInt();
-    for (int i = 0; i < numReduceTasks; i++) {
-      dataVolume[i] = in.readInt();    	
-    }
+//    int numReduceTasks = in.readInt();
+//    for (int i = 0; i < numReduceTasks; i++) {
+//      dataVolume[i] = in.readInt();    	
+//    }
   }
 
   @Override
