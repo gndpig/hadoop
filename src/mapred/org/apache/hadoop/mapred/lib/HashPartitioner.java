17b9b72a2cf55c2ac9e74dd4984a3fcdd277607f
gndpig <gnd.scandal.pig@gmail.com>
2014-12-01 01:38:45 +0900

    修正

--- src/mapred/org/apache/hadoop/mapred/lib/HashPartitioner.java
+++ src/mapred/org/apache/hadoop/mapred/lib/HashPartitioner.java
@@ -21,14 +21,9 @@ package org.apache.hadoop.mapred.lib;
 import org.apache.hadoop.mapred.Partitioner;
 import org.apache.hadoop.mapred.JobConf;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 /** Partition keys by their {@link Object#hashCode()}. 
  */
 public class HashPartitioner<K2, V2> implements Partitioner<K2, V2> {
-
-  public static final Log LOG = LogFactory.getLog(HashPartitioner.class);
 
   public void configure(JobConf job) {}
 
