d6fcc22db39c92f829f33b41a3cf5f1d285499b3
gndpig <gnd.scandal.pig@gmail.com>
2014-12-28 03:07:37 +0900

    ラックの設定

---
+++ conf/rack-awareness.sh
@@ -0,0 +1,18 @@
+HADOOP_CONF=/home/services/hadoop/hadoop/conf
+while [ $# -gt 0 ] ; do
+  nodeArg=$1
+  exec< ${HADOOP_CONF}/topology.data 
+  result="" 
+  while read line ; do
+    ar=( $line ) 
+    if [ "${ar[0]}" = "$nodeArg" ] ; then
+      result="${ar[1]}"
+    fi
+  done 
+  shift 
+  if [ -z "$result" ] ; then
+    echo -n "/default/rack "
+  else
+    echo -n "$result "
+  fi
+done
